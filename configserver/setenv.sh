3ca003396ecd62a92a261de76139087a102c4b3a
daryo <esteban.maruri.tm@transmilenio.gov.co>
2020-07-30 10:54:21 -0500

    a

--- configserver/setenv.sh
+++ configserver/setenv.sh
@@ -1,5 +1,5 @@
 export na="${dpip}"
 export version="v1.0.0"
 export pna="${dbpasswd}"
-export JAVA_OPTS="-Xms8192m -Xmx8192m -XX:MetaspaceSize=8192M -XX:-UseGCOverheadLimit -Djava.net.preferIPv4Stack=true -Duser.timezone=America/Bogota -Ddbpasswd=${pna} -Ddbip=${na}"
+export JAVA_OPTS="-Xms8192m -Xmx8192m -XX:MetaspaceSize=8192M -XX:-UseGCOverheadLimit -Djava.net.preferIPv4Stack=true -Duser.timezone=America/Bogota -Ddbpasswd=${pna} -Ddbip=${na} -Dversion=${version}"
 export CATALINA_OPTS="-Xms8192m -Xmx8192m -XX:MetaspaceSize=8192M -XX:-UseGCOverheadLimit -Djava.net.preferIPv4Stack=true"
